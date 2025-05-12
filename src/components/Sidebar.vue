16372d70305dd723a1881e820c93ca35148a2dc2
manasabhimadolu <bhimadolumanasababy@gmail.com>
2025-05-12 12:45:52 +0530

    code refactor

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,27 @@
 <script setup>
 import LogoImage from '@/assets/images/DashStack.png'
+
+const menuItems = [
+  { label: 'Dashboard', icon: ['fas', 'store'] },
+  { label: 'Products', icon: ['fas', 'box'] },
+  { label: 'Favorites', icon: ['fas', 'heart'] },
+  { label: 'Inbox', icon: ['fas', 'message'] },
+  { label: 'Order Lists', icon: ['fas', 'list'] },
+  { label: 'Product Stock', icon: ['fas', 'box'] },
+  { divider: true },
+  { header: 'PAGES' },
+  { label: 'Pricing', icon: ['fas', 'tag'] },
+  { label: 'Calendar', icon: ['fas', 'calendar'] },
+  { label: 'To-Do', icon: ['fas', 'clipboard-list'] },
+  { label: 'Contact', icon: ['fas', 'phone'] },
+  { label: 'Invoice', icon: ['fas', 'file-invoice'] },
+  { label: 'UI Elements', icon: ['fas', 'chart-bar'] },
+  { label: 'Team', icon: ['fas', 'users'] },
+  { label: 'Table', icon: ['fas', 'table'] },
+  { divider: true },
+  { label: 'Settings', icon: ['fas', 'cog'] },
+  { label: 'Logout', icon: ['fas', 'sign-out-alt'] },
+]
 </script>
 
 <template>
@@ -8,73 +30,16 @@ import LogoImage from '@/assets/images/DashStack.png'
       <img :src="LogoImage" alt="Dashboard" />
     </div>
     <ul class="sidebar_list">
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'store']" />
-        Dashboard
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'box']" />
-        Products
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'heart']" />
-        Favorites
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'message']" />
-        Inbox
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'list']" />
-        Order Lists
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'box']" />
-        Product Stock
-      </li>
-      <n-divider />
-      <li class="sidebar_list_header">PAGES</li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'tag']" />
-        Pricing
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'calendar']" />
-        Calendar
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'clipboard-list']" />
-        To-Do
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'phone']" />
-        Contact
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'file-invoice']" />
-        Invoice
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'chart-bar']" />
-        UI Elements
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'users']" />
-        Team
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'table']" />
-        Table
-      </li>
-      <n-divider />
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'cog']" />
-        Settings
-      </li>
-      <li class="sidebar_list_navitem">
-        <font-awesome-icon class="sidebar_list_navitem_icon" :icon="['fas', 'sign-out-alt']" />
-        Logout
-      </li>
+      <template v-for="(item, index) in menuItems" :key="index">
+        <n-divider v-if="item.divider" />
+        <li v-else-if="item.header" class="sidebar_list_header">{{ item.header }}</li>
+        <li v-else class="sidebar_list_navitem">
+          <div class="sidebar_list_wrapper">
+            <font-awesome-icon class="sidebar_list_wrapper_navitem_icon" :icon="item.icon" />
+            <span class="sidebar_list_wrapper_navitem_label">{{ item.label }}</span>
+          </div>
+        </li>
+      </template>
     </ul>
   </div>
 </template>
