c761e278c0d7483d43c8618f27b741bc4b06ce2f
manasabhimadolu <bhimadolumanasababy@gmail.com>
2025-05-16 20:24:45 +0530

    router setup

---
+++ src/Views/DashBoard.vue
@@ -0,0 +1,32 @@
+<script setup>
+import TopNavbar from '../components/TopNavbar.vue'
+import StatCard from '../components/StatCard.vue'
+import SalesChart from '../components/SalesChart.vue'
+import Table from '../components/Table.vue'
+import { ref, provide, onMounted } from 'vue'
+import SideBar from '../components/SideBar.vue'
+
+const isCollapsed = ref(false)
+
+const toggleCollapse = () => {
+  isCollapsed.value = !isCollapsed.value
+  console.log(isCollapsed?.value)
+}
+
+provide('isCollapsed', isCollapsed)
+provide('toggleCollapse', toggleCollapse)
+</script>
+
+<template>
+  <div class="page-container">
+    <SideBar />
+    <div class="layout">
+      <TopNavbar />
+      <div :class="['main-content', { isCollapsed: isCollapsed }]">
+        <StatCard />
+        <SalesChart />
+        <Table />
+      </div>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import './assets/styles/main.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import naive from 'naive-ui'
+import { createRouter, createWebHistory } from 'vue-router'
+import DashBoard from './Views/DashBoard.vue'
 
 const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'dashboard',
+      component: DashBoard,
+    },
+  ],
+})
+
+app.use(router)
 
 app.use(naive)
 
