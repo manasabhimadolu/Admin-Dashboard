e99db89dc0d709ca74ca4af1d865fcfc075bfc00
manasabhimadolu <bhimadolumanasababy@gmail.com>
2025-05-19 09:39:10 +0530

    responsiveness

--- src/Views/DashBoard.vue
+++ src/Views/DashBoard.vue
@@ -3,15 +3,29 @@ import TopNavbar from '../components/TopNavbar.vue'
 import StatCard from '../components/StatCard.vue'
 import SalesChart from '../components/SalesChart.vue'
 import Table from '../components/Table.vue'
-import { ref, provide, onMounted } from 'vue'
+import { ref, provide, onMounted, onBeforeUnmount } from 'vue'
 import SideBar from '../components/SideBar.vue'
 
 const isCollapsed = ref(false)
+const collapseBreakpoint = 992
 
 const toggleCollapse = () => {
   isCollapsed.value = !isCollapsed.value
   console.log(isCollapsed?.value)
 }
+
+const checkWindowSize = () => {
+  isCollapsed.value = window.innerWidth < collapseBreakpoint
+}
+
+onMounted(() => {
+  checkWindowSize()
+  window.addEventListener('resize', checkWindowSize)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', checkWindowSize)
+})
 
 provide('isCollapsed', isCollapsed)
 provide('toggleCollapse', toggleCollapse)
